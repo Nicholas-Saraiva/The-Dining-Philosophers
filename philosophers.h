b86fd7ba72503a7a64a44106ade9c73328d4cd89
CafeComLag <nicholas.saraivadev@gmail.com>
2025-12-11 20:06:08 +0000

    ADD: Initial struct logic,.

--- philosophers.h
+++ philosophers.h
@@ -8,4 +8,23 @@
 #include <stdio.h>
 #include <threads.h>
 #include <unistd.h>
+
+typedef stuct s_philo
+{
+	int	meal_count;
+	int	last_time_meal;
+	int	*l_fork;
+	int	*r_fork;
+}	t_philo
+
+typedef struct s_table
+{
+	int		meal_max;
+	int		time_to_eat;
+	int		time_to_die;
+	int		time_to_sleep;
+	int		n_seats;
+	int		*forks;
+	t_philo	*philos;
+}	t_table;
 #endif
